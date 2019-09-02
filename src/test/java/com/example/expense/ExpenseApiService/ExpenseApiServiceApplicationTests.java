d304241b7b974d9277e6e2262110551118179087
LeoDubey <saurabh.dubey97666@gmail.com>
2019-09-02 18:38:13 +0530

    expenseserviceapi

---
+++ src/test/java/com/example/expense/ExpenseApiService/ExpenseApiServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.expense.ExpenseApiService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpenseApiServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
